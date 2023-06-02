3b894ef8d9b7edc1f42f691be315027f1e0006d8
jm <email@email.com>
2023-06-02 11:20:03 +0300

    Update ListView View

--- SwiftUI Reference.swiftpm/ListView.swift
+++ SwiftUI Reference.swiftpm/ListView.swift
@@ -5,11 +5,58 @@
 //  Created by JM Velarde on 6/2/23.
 //
 
+//WAYS TO USE List View
+
 import SwiftUI
 
 struct ListView: View {
+    let numArr = ["One", "Two", "Three"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        List {
+            Group {
+                Section {
+                    Text("Text 1")
+                    Text("Text 2")
+                    Text("Text 3")
+                    
+                } header: {
+                    Text("Static")
+                }
+                
+                Section {
+                    ForEach(1..<9) {
+                        Text("Text: \($0)")
+                    }
+                } header: {
+                    Text("Using ForEach")
+                }
+                
+                Section {
+                    Text("This cannot be used under another list")
+                    List(1..<5) {
+                        Text("Dynamic Row \($0)")
+                    }
+                } header: {
+                    Text("Passing range to List(range)")
+                }
+            }.textCase(nil)
+        }
+        
+//        DYNAMIC ROWS
+        List(1..<5) {
+            Text("Dynamic Row \($0)")
+        }.navigationTitle("Test")
+        
+        
+//        USING ARRAYS
+        List(numArr, id: \.self) {
+            Text("\($0)")
+        }
+        
+        
+        
     }
 }
 
@@ -18,3 +65,6 @@ struct ListView_Previews: PreviewProvider {
         ListView()
     }
 }
+
+
+
